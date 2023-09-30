20d1e8dabc80b73938c495ca836cee12ed991e83
ShotaAk <macakasit@gmail.com>
2023-09-30 22:00:31 +0900

    led制御タスクとrobot_info書き込みタスクを作成

--- vssl_main/vssl_main.ino
+++ vssl_main/vssl_main.ino
@@ -14,6 +14,8 @@
 
 #include <M5Unified.h>
 #include <Wire.h>
+#include <map>
+#include <functional>
 
 #include "command_receiver.h"
 #include "robot_information.hpp"
@@ -26,6 +28,91 @@ const IPAddress SUBNET(255,255,255,0);
 constexpr unsigned int PORT = 10003;
 constexpr unsigned int I2C_CLOCK = 400000;
 constexpr unsigned char SLAVE_ADDR = 0x54;
+
+constexpr unsigned int IO_LED = GPIO_NUM_26;
+
+RobotInformations receive_command() {
+  RobotInformations robot_info;
+
+  if(g_receiver.receive()) {
+    // M5_LOGD("Received command!");
+  }
+
+  RobotControl command = g_receiver.get_latest_command();
+
+  robot_info.setRobotVelocity(command.move_velocity.forward, command.move_velocity.left, command.move_velocity.angular);
+
+  // チャージフラグは常時オンする必要あり
+  robot_info.setChargeFlag(true);
+  if (command.kick_speed > 0.0) {
+    robot_info.setKickFlag(true);
+  } else {
+    robot_info.setKickFlag(false);
+  }
+
+  return robot_info;
+}
+
+bool write_robot_info(RobotInformations & robot_info) {
+  const int DATA_SIZE = 7;
+  unsigned char send_data[DATA_SIZE];
+  robot_info.makeCommunicateData();
+  robot_info.getCommunicateData(send_data);
+
+  Wire1.beginTransmission(SLAVE_ADDR);
+  Wire1.write(send_data, DATA_SIZE);
+  
+  if(Wire1.endTransmission() == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+enum class LED_MODE {
+  OFF = 0,
+  CONTINUOUS,
+  BLINK_1HZ,
+};
+static LED_MODE g_led_mode = LED_MODE::OFF;
+
+void led_off() {
+  digitalWrite(IO_LED, LOW);
+}
+
+void led_continuous() {
+  digitalWrite(IO_LED, HIGH);
+}
+
+void led_blink_1hz() {
+  digitalWrite(IO_LED, HIGH);
+  delay(500);
+  digitalWrite(IO_LED, LOW);
+  delay(500);
+}
+
+void led_control_task(void * arg) {
+  std::map<LED_MODE, std::function<void()>> led_mode_map = {
+    {LED_MODE::OFF, led_off},
+    {LED_MODE::CONTINUOUS, led_continuous},
+    {LED_MODE::BLINK_1HZ, led_blink_1hz},
+  };
+
+  while(true) {
+    led_mode_map[g_led_mode]();
+    ets_delay_us(10000);
+  }
+}
+
+RobotInformations g_robot_info;
+void write_robot_info_task(void * arg) {
+  while(true) {
+    if (!write_robot_info(g_robot_info)) {
+      M5_LOGI("Failed to write command.");
+    }
+    ets_delay_us(1000);
+  }
+}
 
 void setup() {
   // 内部I2Cの通信を停止する
@@ -42,7 +129,6 @@ void setup() {
   M5.Log.setLogLevel(m5::log_target_serial, ESP_LOG_INFO);
   M5.Log.setEnableColor(m5::log_target_serial, true);
 
-
   if(!connect_wifi_via_smart_config(IP, IP, SUBNET)) {
     M5_LOGI("Failed to connect Wi-Fi. Reset.");
     ESP.restart();
@@ -54,42 +140,25 @@ void setup() {
   }
 
   Wire1.begin(M5.In_I2C.getSDA(), M5.In_I2C.getSCL(), I2C_CLOCK);
+  pinMode(IO_LED, OUTPUT);
+
+  xTaskCreateUniversal(led_control_task, "led_control_task", 4096, NULL, 1, NULL, CONFIG_ARDUINO_RUNNING_CORE);
+  xTaskCreateUniversal(write_robot_info_task, "write_robot_info_task", 4096, NULL, 2, NULL, CONFIG_ARDUINO_RUNNING_CORE);
 
   M5_LOGI("Hello, world!");
 }
 
+
 void loop() {
   M5.update();
 
-  if(g_receiver.receive()) {
-    // M5_LOGD("Received command!");
+  if(M5.BtnA.isPressed()) {
+    g_led_mode = LED_MODE::CONTINUOUS;
+  } else if(M5.BtnB.isPressed()) {
+    g_led_mode = LED_MODE::BLINK_1HZ;
+  } else if(M5.BtnC.isPressed()) {
+    g_led_mode = LED_MODE::OFF;
   }
 
-  RobotControl command = g_receiver.get_latest_command();
-
-  RobotInformations robot_info;
-  robot_info.setRobotVelocity(command.move_velocity.forward, command.move_velocity.left, command.move_velocity.angular);
-
-  // チャージフラグは常時オンする必要あり
-  robot_info.setChargeFlag(true);
-  if (command.kick_speed > 0.0) {
-    robot_info.setKickFlag(true);
-  } else {
-    robot_info.setKickFlag(false);
-  }
-
-  const int DATA_SIZE = 7;
-  unsigned char send_data[DATA_SIZE];
-  robot_info.makeCommunicateData();
-  robot_info.getCommunicateData(send_data);
-
-  Wire1.beginTransmission(SLAVE_ADDR);
-  Wire1.write(send_data, DATA_SIZE);
-  
-  if(Wire1.endTransmission() == 0) {
-    // M5_LOGI("Sent command!");
-  } else {
-    M5_LOGD("Failed to send command!");
-  }
-
+  g_robot_info = receive_command();
 }
